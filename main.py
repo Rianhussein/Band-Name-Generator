be2cb8deb8ad2711d658455a2e1340de8f4a2f93
rian <rayanhussein101@gmail.com>
2022-09-17 17:12:34 +0300

    Updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print(" Welcome to the band name generator!")
 city = input(" What is the name of the city that you grew up in?\n")
-music = input(" What is your favirute type of music?\n")
+music = input(" What is your favourite type of music?\n")
 print ("Your band name could be:" + city + " " + music)
